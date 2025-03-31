8b6569ecd2dbab3348ded11bee8e0a587785ef85
Shree Hariesh <shreehariesh29@gmail.com>
2025-03-31 16:31:33 +0530

    Template and layout, variable polation

--- Project1/home/views.py
+++ Project1/home/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "home/index.html")
+    title = "Django Practice pro"
+    return render(request, "home/index.html", {"title" : title})
     # return HttpResponse("Welcome to Home Page !")
     # return render(request, 'home/home.html')
 
